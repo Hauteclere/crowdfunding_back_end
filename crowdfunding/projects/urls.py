e9d5c2325667076138cf98daee42275e4a7f2d2a
Hauteclere <oliver.f.lavers@gmail.com>
2023-07-28 17:16:00 +1000

    working

---
+++ crowdfunding/projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+	path('', views.ProjectList.as_view()),
+    path('<int:pk>/', views.ProjectDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
